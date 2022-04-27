c089684657f3fd08bdadf9366faa79b48c14dca8
manu <napestershine@gmail.com>
2022-04-27 08:42:12 +0200

    adapted variables

--- ecs.tf
+++ ecs.tf
@@ -6,10 +6,10 @@ data "template_file" "myapp" {
   template = file("./templates/ecs/api.json.tpl")
 
   vars = {
-    app_image      = "nginx"
-    app_port       = 3000
-    fargate_cpu    = 50
-    fargate_memory = 1024
+    app_image      = var.app_image
+    app_port       = var.app_port
+    fargate_cpu    = var.fargate_cpu
+    fargate_memory = var.fargate_memory
     aws_region     = var.aws_region
   }
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,28 @@ variable "aws_region" {
   description = "The AWS region, in which the infrastructure will be created."
   default     = "us-east-1"
 }
+
+variable "app_image" {
+  description = "Docker image to run in ECS cluster"
+  default = "nginx:latest"
+}
+
+variable "app_port" {
+  description = "Port exposed by docker image"
+  default = 80
+}
+
+variable "app_count" {
+  description = "Count of docker containers to run in ECS Fargate cluster"
+  default = 1
+}
+
+variable "fargate_cpu" {
+  description = "AWS Fargate instance CPU units to provision (1vCPU = 1024 CPU Units)"
+  default = "1024"
+}
+
+variable "fargate_memory" {
+  description = "AWS Fargate instance memory to provision in MB"
+  default = "1024"
+}
